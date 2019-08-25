f9e14be4a7b686da7cfc78bc616aa64e5609d1d1
Thanh Binh <ttbinh@hueic.edu.vn>
2019-08-25 22:39:06 +0700

    Update config 1

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+// mix.js('resources/js/app.js', 'public/js')
+//     .sass('resources/sass/app.scss', 'public/css');
+
+mix.setPublicPath('public/assets')
+    .setResourceRoot('../')
+    .js('resources/assets/js/app.js', 'public/assets/js')
+    .sass('resources/assets/sass/app.scss', 'public/assets/css')
+    .sass('resources/assets/sass/web/web.scss', 'public/assets/css/web')
+    .sass('resources/assets/sass/admin/admin.scss', 'public/assets/css/admin');
